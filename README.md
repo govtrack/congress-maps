603331e8a245de6fbd4deff708b8375078fd2aaa
Joshua Tauberer <jt@occams.info>
2022-12-23 18:14:54 -0500

    Use American Redistricting Project data for the 118th Congress until the Census Bureau publishes its authoritative data

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Mapping US Congressional Districts
 
-Follow the steps below to create a web map of United States congressional districts from Census Bureau data using Mapbox. You can also use this to create a lat/lng-to-congressional district API.
+These instructions create maps for the 118th Congress using district boundaries as of the 2022 elections.)
 
-These instructions create maps for the 116th Congress using district boundaries as of the 2018 elections. (These differed from the 115th Congress boundaries because of court-ordered redistricting in Pennsylvania in 2018.)
+NOTE: Typically we use Census Bureau data but to get ahead of the game for the 118th Congress, since Census data is not yet published for the new districts, these instructions use congressional district geospatial data collected by the [American Redistricting Project](https://thearp.org/blog/map-archive/) as combined into a single file by [Glenn Rice](https://acsdatacommunity.prb.org/discussion-forum/f/forum/977/shapefiles-for-118th-cds-current-slds) on June 8, 2022. ARP doesn't include boundaries for DC and the island territories, so they are added from the Census's most recent congressional district shapefiles.
+
+---
+
+Follow the steps below to create a web map of United States congressional districts from [Census Bureau geospatial data](https://www.census.gov/programs-surveys/geography/technical-documentation/user-note/cd-sld-note.html) using Mapbox to render the web map. You can also use this to create a lat/lng-to-congressional district API using the Mapbox API.
 
 You will need an account on Mapbox.com. Then follow the commands below from the Mac OS X or Ubuntu terminal.
 
@@ -45,45 +49,51 @@ npm install
 
 #### Creating the map:
 
-To complete these steps, run the commands below. Set `MAPBOX_USERNAME` to your Mapbox username, `MAPBOX_DEFAULT_ACCESS_TOKEN` to your Mapbox default public token, and `MAPBOX_ACESS_TOKEN` to a `uploads:write` scope access token from your [Mapbox account](https://www.mapbox.com/studio/account/tokens).
+To complete these steps, run the commands below.
 
-```
+```sh
 # create directory to store data
 mkdir data
 
-# dowload Census boundaries data, unzip the data, and convert it to GeoJSON
-wget -P data ftp://ftp2.census.gov/geo/tiger/TIGER2018/CD/tl_2018_us_cd116.zip
-unzip data/tl_2018_us_cd116.zip -d ./data/
-ogr2ogr -f GeoJSON -t_srs crs:84 data/congressional_districts.geojson data/tl_2018_us_cd116.shp
+# dowload district boundary data, unzip the data, and convert it to GeoJSON
+wget -P data ftp://ftp2.census.gov/geo/tiger/TIGER2022/CD/tl_2022_us_cd116.zip
+unzip data/tl_2022_us_cd116.zip -d ./data/
+ogr2ogr -f GeoJSON -t_srs crs:84 data/congressional_districts_116.geojson data/tl_2022_us_cd116.shp
 
-# download new 2020 North Carolina CDs
-# (h/t https://blogs.sas.com/content/graphicallyspeaking/2019/11/25/plotting-ncs-new-congressional-districts-maps-for-2020/)
-wget -O data/NC_HB1029_3.zip https://webservices.ncleg.gov/ViewBillDocument/2019/6953/0/HB%201029,%203rd%20Edition%20-%20Shapefile
-unzip data/NC_HB1029_3.zip -d ./data/
-ogr2ogr -f GeoJSON -t_srs crs:84 data/congressional_districts_nc.geojson data/C-Goodwin-A-1-TC.shp
+wget -P data https://mcdc.missouri.edu/data/georef/leg_districts_2022/USA_CD118.zip
+unzip data/USA_CD118.zip -d ./data/
+ogr2ogr -f GeoJSON -t_srs crs:84 data/congressional_districts.geojson data/USA_CD118.shp
 
-# run processing on data
+# normalize format and add label points, saving to data/map.geojson and data/bboxes.js
 node process.js
 
 # create Mapbox vector tiles from data
-tippecanoe/tippecanoe -o data/cd-117-2020.mbtiles -f -Z 0 -z 12 -B0 -pS -n "117th Congress (2020 Election) Congressional Districts" data/map.geojson
+tippecanoe/tippecanoe \
+	-f -Z 0 -z 12 -B0 -pS \
+	-o data/cd-118-2022-arp.mbtiles \
+	--name "118th Congress (2022 Election) Congressional Districts - American Redistricting Project Data" \
+	data/map.geojson
+```
 
+For the last step to upload the map boundaries to Mapbox, set `MAPBOX_USERNAME` to your Mapbox username, `MAPBOX_DEFAULT_ACCESS_TOKEN` to your Mapbox default public token, and `MAPBOX_ACESS_TOKEN` to a `uploads:write` scope access token from your [Mapbox account](https://www.mapbox.com/studio/account/tokens).
+
+```sh
 # setup Mapbox account name, default public token, and write-scoped token
 export MAPBOX_USERNAME=<your mapbox username>
 export MAPBOX_DEFAULT_ACCESS_TOKEN=<your mapbox default access token>
 export MAPBOX_WRITE_SCOPE_ACCESS_TOKEN=<your mapbox write scope access token>
 
 # upload map data to Mapbox.com
-node upload.js data/cd-117-2020.mbtiles "cd-117-2020" "US_Congressional_Districts_117th_Congress_2020_Elections"
+node upload.js data/cd-118-2022-arp.mbtiles "cd-118-2022-arp" "US_Congressional_Districts_118th_Congress_2022_Election_ARP"
 ```
 
 Check out [mapbox.com/studio](https://www.mapbox.com/studio) to see updates on data processing. Once Mapbox is finished processing your upload, you can make a map style. Click New Style. Choose a template --- I last tried Navigation - Day, which seemed nice. Then create layers:
 
-* Create a new layer. In Select Data, Select the uploaded tileset as the source. Change the type to fill. In Style, click Style With Data Conditions. Select color_index as the data value. Select 0, choose a color, click Done. Then click Add Another Condition and repeat for 1 through 4. Name the layer CD-Fills. Drag the layer all the way to the bottom of the layers but above "Land & water, land" so that it covers the base color for land but is behind all of the other map features. Click Style Across Zoom Range. Click the stop point for zoom 22 and set the opacity for each color to 0 and the zoom level to 15, so that the fills fade out when zoomed into street level.
-* Create another layer with the same data named CD-Outlines. Set its type to Line. Click Width, then Style Across Zoom Range. Set the first zoom stop to zoom level 3 and line width 1px. Set the second zoom stop to zoom level 15 and the line width to 4px. Keep this layer at the top.
-* Create another layer named CD-Labels. Set its type to Symbol. Click Text Field, style across zoom range. At zoom stop 0, set the text field to the data field title_short. Set the second stop to zoom level 7 and the text field to the data field title_long. I set the font to Montserrat Bold 17px.
-
-I turned off country-label and state-label visibility.
+* Create a new layer and select the uploaded tileset as the source. Change the type to fill. In Style - Color, click Style With Data Conditions. Select color_index as the data value. Select 0, choose a color, click Done. Then click Add Another Condition and repeat for 1 through 4. Click Opacity, Style Across Zoom Range. Click the stop point for zoom 22 and set the opacity to 0 and the zoom level to 15, so that the fills fade out when zoomed into street level. Name the layer CD-Fills. Drag the layer all the way to the bottom of the layers but above "Land & water, land" so that it covers the base color for land but is behind all of the other map features. 
+* Create another layer with the same data named CD-Outlines. Set its type to Line. In Style, click Width, then Style Across Zoom Range. Set the first zoom stop to zoom level 3 and line width 1px. Set the second zoom stop to zoom level 15 and the line width to 4px. Keep this layer at the top.
+* Create another layer named CD-Labels. Set its type to Symbol. Set a filter on `group` to `label`. In Style, click Text Field, style across zoom range. At zoom stop 0, set the text field to the data field title_short. Set the second stop to zoom level 7 and the text field to the data field title_long. I set the font to Montserrat Light 12px at zoom level 0 and Montserrat Bold 16px at zoom level 7. I added 4 px white halo.
+* In the Administrative Boundaries component, I turned off all boundaries.
+* Click Publish.
 
 #### Usage:
 
--- process.js
+++ process.js
@@ -10,71 +10,75 @@ var stateFipsCodesMap = { };
 stateCodes.forEach(function(item) { stateFipsCodesMap[item.FIPS] = item; })
 fs.writeFileSync('./example/states.js', 'var states = ' + JSON.stringify(stateCodes, null, 2));
 
+// turns 1 into '1st', etc.
+function ordinal(number) {
+  var suffixes = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
+  if (((number % 100) == 11) || ((number % 100) == 12) || ((number % 100) == 13))
+    return number + suffixes[0];
+  return number + suffixes[number % 10];
+}
+
 // Load and re-format the congressional district data.
 var census_boundaries = 
   JSON.parse(fs.readFileSync("data/congressional_districts.geojson", 'utf8'))
   .features
-  .filter(function(d) {
-    // Some states have district 'ZZ' which represents the area of
-    // a state, usually over water, that is not included in any
-    // congressional district --- filter these out
-    if (d.properties['CD116FP'] == 'ZZ')
-      return false;
-
-    // Remove NC's districts which were revised and are loaded
-    // from a separate file.
-    if (d.properties['STATEFP'] == 37)
-      return false;
-
-    return true;
-  })
-  // re-do the Feature's properties to our own format
   .map(function(item) {
+    // Get state from FIPS code.
+    let stateinfo = stateFipsCodesMap[parseInt(item.properties.state)];
+    stateinfo.seen = true; // did we get boundaries for every state?
+
+    // Get the district number in two-digit form: "00" for at-large
+    // districts, "01", "02", .... The Census data's CD___FP field
+    // holds it in this format, except for the island territories
+    // which have "98", but are just at-large and should be "00".
+    let district_number = item.properties.CD118;
+    if (district_number == "98") district_number = "00";
+    if (district_number == "AL") district_number = "00"; // American Redistricting Project
+
     return {
       "type": "Feature",
       "properties": {
-        // Convert from FIPS code.
-        state: stateFipsCodesMap[parseInt(item.properties.STATEFP)].USPS,
-        state_name: stateFipsCodesMap[parseInt(item.properties.STATEFP)].Name,
-
-        // Get the district number in two-digit form ("00" (for at-large
-        // districts), "01", "02", ...). The Census data's CD116FP field
-        // holds it in this format. Except for the island territories
-        // which have "98", but are just at-large and should be "00".
-        number: item.properties.CD116FP == "98" ? "00" : item.properties.CD116FP,
-
-        // Census TIGER files have INTPTLON/INTPTLAT which conveniently
-        // provides a point where a label for the polygon can be placed.
-        // But these points have inconsistent quality in terms of usage
-        // for labels, and some are very poorly located, so we don't use them
-        // unless the alternate method of locating a label point fails.
-        label_pt_lon: parseFloat(item.properties.INTPTLON),
-        label_pt_lat: parseFloat(item.properties.INTPTLAT),
+        state: stateinfo.USPS,
+        state_name: stateinfo.Name,
+        number: district_number,
+        title_short: stateinfo.USPS + ' ' + (district_number == "00" ? "At Large" : parseInt(district_number)),
+        title_long: stateinfo.Name + '’s ' + (district_number == "00" ? "At Large" : ordinal(parseInt(district_number))) + ' Congressional District',
       },
       "geometry": item.geometry
     };
   });
 
-// Add NC's districts.
-census_boundaries = 
-  census_boundaries.concat(
-  JSON.parse(fs.readFileSync("data/congressional_districts_nc.geojson", 'utf8'))
+// Fill in the territories that weren't present in the ARP data.
+JSON.parse(fs.readFileSync("data/congressional_districts_116.geojson", 'utf8'))
   .features
-  .map(function(item) {
-    return {
+  .filter(function(d) {
+    // Some states have district 'ZZ' which represents the area of
+    // a state, usually over water, that is not included in any
+    // congressional district --- filter these out.
+    if (d.properties['CD116FP'] == 'ZZ')
+      return false;
+    return true;
+  })
+  .forEach(item => {
+    // Skip states we've already seen.
+    let stateinfo = stateFipsCodesMap[parseInt(item.properties.STATEFP)];
+    if (stateinfo.seen) return;
+
+    // The territories all have at-large delegates.
+    district_number = "00";
+
+    census_boundaries.push({
       "type": "Feature",
       "properties": {
-        state: "NC",
-        state_name: stateFipsCodesMap[37].Name,
-
-        // Get the district number in two-digit form ("00" (for at-large
-        // districts), "01", "02", ...).
-        number: (item.properties.DISTRICT.length == 1 ? '0' : '')
-          + item.properties.DISTRICT,
+        state: stateinfo.USPS,
+        state_name: stateinfo.Name,
+        number: district_number,
+        title_short: stateinfo.USPS + ' ' + (district_number == "00" ? "At Large" : parseInt(district_number)),
+        title_long: stateinfo.Name + '’s ' + (district_number == "00" ? "At Large" : ordinal(parseInt(district_number))) + ' Congressional District',
       },
       "geometry": item.geometry
-    };
-  }));
+    });
+  });
 
 // Build a new FeatureCollection that we can pass into fiveColorMap.
 var districts = {
@@ -96,37 +100,42 @@ districts.features.forEach(function(feature) {
   delete feature.properties.fill;
 });
 
-// turns 1 into '1st', etc.
-function ordinal(number) {
-  var suffixes = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
-  if (((number % 100) == 11) || ((number % 100) == 12) || ((number % 100) == 13))
-    return number + suffixes[0];
-  return number + suffixes[number % 10];
-}
-
 // Create a new empty FeatureCollection to contain final map data that
 // contains both district boundaries and label points.
 var mapData = { 'type': 'FeatureCollection', 'features': [] }
 districts.features.forEach(function(d) {
-  // Compute a good location to place a label for this district. Use the
-  // polygon with the largest area.
-  var label_coord = polylabel(d.geometry.coordinates, .01);
-  if (Number.isNaN(label_coord[0])) {
-    // polylabel fails for a variety of districts for unknown reasons.
-    // Fall back to the Census interior points.
-    label_coord = [d.properties.label_pt_lon, d.properties.label_pt_lat];
-    //console.log(d.properties.state, d.properties.number, label_coord);
-  }
+  // Compute a good location to place a label for this district.
+  // If the district has multiple parts, use the largest part.
+  // polylabel doesn't work with a MultiPolygon and also putting
+  // the label in the largest part probably will look best.
+  let d_label = d;
+  if (d_label.geometry.type == "MultiPolygon") {
+    // Split the MultiPolygon into Polygon features and use
+    // turf.area to compute each's area. Find the one with
+    // the largest area.
+    d_label = null;
+    d.geometry.coordinates.forEach(geom => {
+      let polygon = {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: geom
+        }
+      };
+      polygon.area = turf.area(polygon);
+      if (d_label === null || polygon.area > d_label.area)
+        d_label = polygon;
+    });
+  };
+  var label_coord = polylabel(d_label.geometry.coordinates, 1);
+  if (Number.isNaN(label_coord[0]))
+    throw d.properties.title_long;
 
   // Create a turf.point to hold information for rending labels.
   var pt = turf.point(label_coord);
 
-  // add metadata to the label
+  // copy district metadata to the label
   pt.properties = JSON.parse(JSON.stringify(d.properties)); // copy hack to avoid mutability issues
-  pt.properties.title_short = d.properties.state + ' ' + (d.properties.number == "00" ? "At Large" : parseInt(d.properties.number));
-  pt.properties.title_long = d.properties.state_name + '’s ' + (d.properties.number == "00" ? "At Large" : ordinal(parseInt(d.properties.number))) + ' Congressional District';
-  delete pt.properties.label_pt_lon;
-  delete pt.properties.label_pt_lat;
 
   // add a type property to distinguish between labels and boundaries
   pt.properties.group = 'label';
